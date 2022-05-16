34eb9d40cfe1c74131b2f5fcdf75f08af2e94496
Kimia Bahrami <kimibhr@Kimias-MacBook.local>
2022-05-16 17:46:07 -0400

    class practice

---
+++ src/GitHubSahar1.java
@@ -0,0 +1,12 @@
+
+public class GitHubSahar1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		
+	System.out.println("this is amazing class");	
+		
+	}
+
+}
